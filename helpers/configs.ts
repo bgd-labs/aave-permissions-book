c11f121f755aab7848331da8931aebf825d07eaf
sendra <polsendra@gmail.com>
2023-08-09 18:19:23 +0200

    fix: added base

--- helpers/configs.ts
+++ helpers/configs.ts
@@ -17,7 +17,7 @@ import {
   AaveV3Optimism,
   AaveV3Polygon,
   AaveV3Metis,
-  // AaveV3Basenet,
+  AaveV3Basenet,
 } from '@bgd-labs/aave-address-book';
 import { NetworkConfigs } from './types.js';
 dotenv.config();
@@ -200,11 +200,11 @@ export const networkConfigs: NetworkConfigs = {
       '0x9e10C0A1Eb8FF6a0AaA53a62C7a338f35D7D9a2A': 'Aave Guardian Base',
     },
     pools: {
-      // [Pools.V3]: {
-      //   aclBlock: 2357130,
-      //   permissionsJson: './statics/functionsPermissionsV3.0.1.json',
-      //   addressBook: AaveV3Basenet,
-      // },
+      [Pools.V3]: {
+        aclBlock: 2357130,
+        permissionsJson: './statics/functionsPermissionsV3.0.1.json',
+        addressBook: AaveV3Basenet,
+      },
     },
   },
   // [ChainId.harmony]: {
