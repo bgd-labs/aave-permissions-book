7030c2ab68c0e0495c3e93a90ca8426b33748d9a
sendra <polsendra@gmail.com>
2023-11-02 12:41:55 +0100

    fix: removed comment

--- scripts/govV3Permissions.ts
+++ scripts/govV3Permissions.ts
@@ -405,7 +405,6 @@ export const resolveGovV3Modifiers = async (
     for (let i = 0; i < supportedChains.length; i++) {
       const bridges: string[] =
         await cccContract.getReceiverBridgeAdaptersByChain(supportedChains[i]);
-      // get bridge name
       bridges.map((bridge) => receiverBridges.add(bridge));
     }
 
